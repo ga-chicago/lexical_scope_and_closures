dc40b917747ab691ffd2db0d3d6928a4b0e195f0
James Traver <jtraverjr@gmail.com>
2016-03-08 11:19:51 -0600

    init

---
+++ app.js
@@ -0,0 +1,8 @@
+'use strict'
+
+{
+  console.log("Today we will discuss Lexical Scope + Closures!");
+  console.log("We will also explore some concepts that live purely in Node.js (not on the Web).");
+  console.log("However, Lexical Scope and Closures exist on the web!");
+  console.log("Are you ready? [Shout YES! when you read this]");
+}
